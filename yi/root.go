7f9321229b427796613e0d53ad98684aebc4bbc0
Chu Yang <chuyang2@gomecloud.com>
2018-10-24 17:07:50 +0800

    Minor code tuning

--- yi/root.go
+++ yi/root.go
@@ -43,10 +43,6 @@ func Execute() {
 
 func init() {
     cobra.OnInitialize()
-
-    // Cobra also supports local flags, which will only run
-    // when this action is called directly.
-    rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // 屏蔽日志信息
--- yi/version.go
+++ yi/version.go
@@ -14,20 +14,20 @@ var (
     BuildTime  string
 )
 
-// versionCmd represents the appVersion command
-var versionCmd = &cobra.Command{
-    Use:   "version",
-    Short: "Show Version",
-    Long:  `Print the appVersion information.`,
-    Run: func(cmd *cobra.Command, args []string) {
-        fmt.Printf("Version: %s\n", AppVersion)
-        fmt.Printf("Git:     %s\n", GitVersion)
-        fmt.Printf("GoC:     %s\n", GoVersion)
-        fmt.Printf("Build:   %s\n", BuildTime)
-        fmt.Printf("Go:      %s\n", runtime.Version())
-    },
-}
-
 func init() {
+    // versionCmd represents the appVersion command
+    var versionCmd = &cobra.Command{
+        Use:   "version",
+        Short: "Show Version",
+        Long:  `Print the appVersion information.`,
+        Run: func(cmd *cobra.Command, args []string) {
+            fmt.Printf("Version: %s\n", AppVersion)
+            fmt.Printf("Git:     %s\n", GitVersion)
+            fmt.Printf("GoC:     %s\n", GoVersion)
+            fmt.Printf("Build:   %s\n", BuildTime)
+            fmt.Printf("Go:      %s\n", runtime.Version())
+        },
+    }
+
     rootCmd.AddCommand(versionCmd)
 }
